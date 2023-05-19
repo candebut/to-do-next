60771801fd14bf0dda59bd09f7f4893b4fd93807
Candela Buttigliero <candelabuttigliero@MacBook-Pro-de-Candela.local>
2023-05-19 16:13:01 +0200

    adding modal for adding a new task

--- my-app/app/components/AddTask.tsx
+++ my-app/app/components/AddTask.tsx
@@ -15,7 +15,21 @@ const AddTask = () => {
         Add task <AiOutlinePlus className="ml-2" size={10} />
       </button>
 
-      <Modal modalOpen={modalOpen} />
+      <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
+        <form>
+          <h3 className="font-bold text-lg">Add new task</h3>
+          <div className="modal-action">
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered w-full "
+            />
+            <button className="btn" type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
+      </Modal>
     </div>
   );
 };
--- my-app/app/components/Modal.tsx
+++ my-app/app/components/Modal.tsx
@@ -1,24 +1,21 @@
 interface ModalProps {
   modalOpen: boolean;
+  setModalOpen: (open: boolean) => boolean | void;
+  children: React.ReactNode;
 }
 
-const Modal: React.FC<ModalProps> = ({ modalOpen }) => {
+const Modal: React.FC<ModalProps> = ({ modalOpen, setModalOpen, children }) => {
   return (
     <div className={`modal ${modalOpen && "modal-open"}`}>
       <div className="modal-box relative">
         <label
           htmlFor="my-modal-3"
           className="btn btn-sm btn-circle absolute right-2 top-2"
+          onClick={() => setModalOpen(false)}
         >
           ✕
         </label>
-        <h3 className="text-lg font-bold">
-          Congratulations random Internet user!
-        </h3>
-        <p className="py-4">
-          You've been selected for a chance to get one year of subscription to
-          use Wikipedia for free!
-        </p>
+        {children}
       </div>
     </div>
   );
