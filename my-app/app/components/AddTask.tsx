bd95e9ccee1edb8e6b29bc333cf75c9794766ff1
Candela Buttigliero <candelabuttigliero@MacBook-Pro-de-Candela.local>
2023-05-19 14:34:23 +0200

    creating todo app: added dependencies, tail, icons, json. working on components

---
+++ my-app/app/components/AddTask.tsx
@@ -0,0 +1,13 @@
+import { AiOutlinePlus } from "react-icons/ai";
+
+const AddTask = () => {
+  return (
+    <div>
+      <button className="btn btn-primary w-full">
+        Add task <AiOutlinePlus className="ml-2" size={10} />
+      </button>
+    </div>
+  );
+};
+
+export default AddTask;
