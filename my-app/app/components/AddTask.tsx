8008ade8ff15aa09e6c1c99afbec1cc978329361
Candela Buttigliero <candelabuttigliero@MacBook-Pro-de-Candela.local>
2023-05-19 17:47:32 +0200

    adding uuid to get unique id for each task

--- my-app/app/components/AddTask.tsx
+++ my-app/app/components/AddTask.tsx
@@ -4,19 +4,23 @@ import { AiOutlinePlus } from "react-icons/ai";
 import Modal from "./Modal";
 import { useState, FormEventHandler } from "react";
 import { addTodo } from "@/api";
+import { useRouter } from "next/navigation";
+import { v4 as uuidv4 } from "uuid";
 
 const AddTask = () => {
+  const router = useRouter();
   const [modalOpen, setModalOpen] = useState<boolean>(false);
   const [newTaskValue, setNewTaskValue] = useState<string>("");
 
   const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
     await addTodo({
-      id: "4",
+      id: uuidv4(),
       text: newTaskValue,
     });
     setNewTaskValue("");
     setModalOpen(false);
+    router.refresh();
   };
 
   return (
